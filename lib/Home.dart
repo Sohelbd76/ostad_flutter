47d5e8e5a88508a68dab32f8657bb500bf23ca4f
Sohelbd76 <asusbd76@gmail.com>
2025-07-08 18:29:40 +0600

    Basic flutter frame

--- lib/Home.dart
+++ lib/Home.dart
@@ -10,7 +10,100 @@ class Home extends StatelessWidget {
       title: const Text('Ostad'),
       centerTitle: true,
        backgroundColor: Colors.amber,shadowColor: Colors.red,
-
+    ),
+    body: SingleChildScrollView(
+    child: Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+      Text(
+        'Test 1',
+        style: TextStyle(
+            fontWeight: FontWeight.w200, color: Colors.grey, fontSize: 20),
+      ),
+      Text('Test 2'),
+      Text(
+        'Test 3',
+        style: TextStyle(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
+      ),
+      Text(
+        'Test 1',
+        style: TextStyle(
+            fontWeight: FontWeight.w200, color: Colors.grey, fontSize: 20),
+      ),
+      Text('Test 2'),
+      Text(
+        'Test 3',
+        style: TextStyle(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
+      ),
+    Text(
+    'Test 1',
+    style: TextStyle(
+    fontWeight: FontWeight.w200, color: Colors.grey, fontSize: 20),
+    ),
+    Text('Test 2'),
+    Text(
+    'Test 3',
+    style: TextStyle(
+    fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
+    ),
+    Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      Text(
+        'Test 1',
+        style: TextStyle(
+            fontWeight: FontWeight.w200, color: Colors.grey, fontSize: 20),
+      ),
+      Text('Test 2'),
+      Text(
+        'Test 3',
+        style: TextStyle(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
+      ),
+      Text(
+        'Test 1',
+        style: TextStyle(
+            fontWeight: FontWeight.w200, color: Colors.grey, fontSize: 20),
+      ),
+      Text('Test 2'),
+      Text(
+        'Test 3',
+        style: TextStyle(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
+      ),
+      Text(
+        'Test 1',
+        style: TextStyle(
+            fontWeight: FontWeight.w200, color: Colors.grey, fontSize: 20),
+      ),
+      Text('Test 2'),
+      Text(
+        'Test 3',
+        style: TextStyle(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
+      ),
+    Text(
+    'Test 4',
+    style: TextStyle(
+    fontWeight: FontWeight.w200, color: Colors.grey, fontSize: 20),
+    ),
+    Text('Test 5'),
+    Text(
+    'Test 6',
+    style: TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    color: Colors.blue),
+    ),
+    ],
+    ),
+    ],
+    ),
     ));
-  }
+
+    }
 }
