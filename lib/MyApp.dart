1501e93e4c7e762be5cb94756f83b64a9cb5b97e
Sohelbd76 <asusbd76@gmail.com>
2025-07-09 13:39:47 +0600

    Basic flutter frame 2

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'Home.dart';
+import 'button.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: Button(),
     );
   }
 }
---
+++ lib/button.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  const Button({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.red.withOpacity(.2),
+
+      appBar: AppBar(
+        title: Text('Ostad'),
+        backgroundColor: Colors.blue.shade300,
+        
+        centerTitle: true,
+
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // MainAxisAlignment.center;
+               SizedBox(height:10),
+            ElevatedButton(onPressed: (){print('Enter');},child: Text('Submit'),style:ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50))),
+
+            ),
+            SizedBox(height: 10),
+            OutlinedButton(onPressed: (){}, child: Text('outline button',style: TextStyle(color: Colors.black,
+             fontSize: 20,fontWeight: FontWeight.bold,
+                ),)
+
+            // FloatingActionButton(onPressed: (){print("Enter");},child: Text('Enter'),),
+            ),
+          SizedBox(height: 10),
+          TextButton(onPressed: (){}, child: Text('Text Buttton',style: TextStyle(fontSize: 20,color: Colors.red),)),
+         IconButton(onPressed: (){}, icon: Icon(Icons.phone_android,size: 30,),color: Colors.red,),
+         SizedBox(height: 10,),
+            GestureDetector(
+             onTap: (){print('Gesture check');},
+             child: Text('Gesture Check',style: TextStyle(fontSize: 20,color: Colors.grey),)),
+            InkWell(
+                onDoubleTap: (){print('After double tab');},
+                child: Text('Inkwell checker',style: TextStyle(fontSize: 30,color: Colors.red),)),
+
+         Padding(
+
+             // padding:EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+           padding: EdgeInsets.only(left: 10,top: 30,right: 10,bottom: 30),
+             child: Text('Padding checking Padding checking Padding checking Padding checking'
+                 'Pa checking Padding checking'
+                 'Padding checking Padding checking Padding checking Padding checking'
+                 'Padding checking Padding checking Padding checking Padding checking'
+                 'Padding checking Padding checking Padding checking Padding checking '))
+
+
+          ],
+        ),
+      )
+    );
+  }
+}
